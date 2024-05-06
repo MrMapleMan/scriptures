ba6adeb2d988cadea289cab1ad02b541b90f644d
John Henrie <gimmeyerjunkmail@gmail.com>
2024-05-06 09:41:31 -0600

    new functions; use pickle with content

--- notes/readnotes.py
+++ notes/readnotes.py
@@ -3,6 +3,7 @@
 #  - Fix use_regex functionality in isContained
 
 import os
+import pickle
 import re
 import pandas as pd
 import sys
@@ -10,8 +11,18 @@ sys.path.insert(0,os.path.realpath(os.path.dirname(__file__)+'/../scriptures'))
 from scriptures import Scriptures
 
 class Notes:
-    def __init__(self):
-        self.df_all_notes = pd.read_csv(os.path.dirname(__file__)+'/LDSNotesDownload.csv')
+    def __init__(self, notes_pickle='notes_with_content_df.pkl'):
+        if notes_pickle[-4:] == '.pkl':
+            with open(notes_pickle,'rb') as f:
+                self.df_all_notes = pickle.load(f)
+        elif notes_pickle[-4:] == '.csv':
+            self.df_all_notes = pd.read_csv(notes_pickle)
+        else:
+            print('Failed to locate notes data file. Using default LDSNotesDownload.csv')
+            self.df_all_notes = pd.read_csv(os.path.dirname(__file__)+'/LDSNotesDownload.csv')
+        if 'contents' not in self.df_all_notes:
+            self.df_all_notes = self.df_all_notes.assign(contents='Content has not been saved for these notes')
+        
         self.df_verses = Scriptures().getDF()
         self.filterTagless()
         self.findAllTags()
@@ -19,7 +30,8 @@ class Notes:
         
     def filterTagless(self):
         self.df_tagged_notes = self.df_all_notes[~self.df_all_notes['tags'].isnull()].copy()
-        self.df_tagged_notes['tags_list'] = self.df_tagged_notes['tags'].apply(lambda x: x.split('; '))
+        self.df_tagged_notes['tags_list'] = self.df_tagged_notes['tags'].apply(lambda x: x.split('; ') if isinstance(x, str) else [])
+        self.df_all_notes['tags_list'] = self.df_all_notes['tags'].apply(lambda x: x.split('; ') if isinstance(x, str) else [])
 
     def findAllTags(self):
          self.all_tags = self.flatten(self.df_tagged_notes['tags_list'].to_list())
@@ -49,13 +61,12 @@ class Notes:
         print('Tags matching regular expression "{:}"'.format(regexp))
         print('Total: {:,}'.format(total))
         for i in matching_tags:
-            print('{:^50} - {:3}'.format(i[0],i[1]))
+            print('{:^65} - {:3}'.format(i[0],i[1]))
+        print()
         
-    def isContained(self,search_for,search_in, regex_bool=False, re_flags=None):
+    def isContained(self,search_for,search_in, regex_bool=False, re_flags=re.NOFLAG):
         # search_for = [i.lower() for i in search_for]
         # search_in  = [i.lower() for i in search_in]
-        if re_flags is None:
-            re_flags = re.NOFLAG
         for idx,j in enumerate(search_for):
             if regex_bool:
                 if True not in [True if re.search(j,val,re_flags) else False for val in search_in]:
@@ -67,8 +78,12 @@ class Notes:
         return False
         
     def printSortedTags(self):        
+        hyphens = '-'*len('| {:5} | {:^65} {:3}        {:^65} {:3} |'.format('','Tags (Alphabetical)','','Tags (Descending By Count)',''))
+        print(hyphens)
+        print('| {:5} | {:^65} {:3} |      {:^65} {:3} |'.format('Index','Tags (Alphabetical)','','Tags (Descending By Count)',''))
+        print(hyphens)
         for i in range(min([len(self.sorted_alphabet), 100])):
-            print('{:5,} {:^53} {:03}        {:^50} {:03}'.format(i, 
+            print('| {:5,} | {:^65} {:03} |      {:^65} {:03} |'.format(i, 
                                                                 self.sorted_alphabet[i][0], 
                                                                 self.sorted_alphabet[i][1], 
                                                                 self.sorted_count[i][0], self.sorted_count[i][1]))
@@ -76,19 +91,30 @@ class Notes:
     def getColumns(self):
         return self.df_all_notes.columns
     
-    def findNotesWithTags(self, search_list, use_regex=False, re_flags=None):
+    def findNotesWithTags(self, search_list, use_regex=False, re_flags=re.NOFLAG):
         matching = self.df_tagged_notes[self.df_tagged_notes['tags_list'].apply(lambda x: self.isContained(search_list, x, regex_bool=use_regex, re_flags=re_flags))]
         return matching
+    
+    def searchContents(self, regex, flags=re.NOFLAG):
+        matching = self.df_all_notes[self.df_all_notes['contents'].apply(lambda x: True if re.search(regex, x, flags) else False)]
+        max_rows = pd.get_option('display.max_rows')
+        pd.set_option('display.max_rows', 750)
+        print(matching)
+        pd.set_option('display.max_rows', max_rows)
+        
 
 if __name__ == '__main__':
     print('John: {}'.format(sys.version))
     notes = Notes()
-    search_for = ['factu','ness']
+    search_for = ['fasdftu','ness']
     search_in = ['factfulness','joiefjfactu']
     print(notes.isContained(search_for, search_in, True))
-    result = notes.findNotesWithTags(['faith','joy|happ'],use_regex=True, re_flags=re.IGNORECASE)[['source location','tags_list']]
+    result = notes.findNotesWithTags(['faith','joy|happ'],use_regex=True, re_flags=re.IGNORECASE)[['source location','tags_list','contents']]
     print(result)
     print(result['tags_list'].to_list())
+    notes.findTagsRegexp('Jesus|Christ\\b(?: \()?|Savior', flags=re.IGNORECASE)
+    notes.printSortedTags()
+    notes.searchContents('ID not found on url ')
     # notes.findTagsRegexp(r'fear|factful',flags=re.IGNORECASE)
     
     sys.exit()
